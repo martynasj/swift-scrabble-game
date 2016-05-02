72434272ca726fc1b6f7dac46e79c472e255c451
martynas <mj@svift.net>
2016-05-02 19:30:57 +0200

    Initial Commit

---
+++ scrabble-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrabble-game
+//
+//  Created by Martynas Jankauskas on 02/05/16.
+//  Copyright © 2016 Martynas Jankauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
